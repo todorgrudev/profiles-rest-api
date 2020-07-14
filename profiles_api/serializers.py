706281fca54d791ef55ec774e03d43bcd8176f4b
Todor <todorgrudev91@gmail.com>
2020-07-14 13:04:01 +0300

    Added login API

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -33,4 +33,4 @@ class UserProfileSerializer(serializers.ModelSerializer):
 		if 'password' in validated_data:
 			password = validated_data.pop('password')
 			instance.set_password(password)
-		return super(),update(instance,validated_data)
+		return super().update(instance,validated_data)
